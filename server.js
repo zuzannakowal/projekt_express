b106de764c52b09af70506d354890a10b14fbdf3
Zuzanna Kowal <zuzulajt@onet.pl>
2020-12-06 13:16:02 +0100

    dodana klasa Users i testy konsolowe

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 "use strict";
+
+const Users = require("./users.js")
+
+let uczniowie = new Users();
+uczniowie.zapiszUzytkownika("aaa", "sss", 24, true, "k")
+uczniowie.zapiszUzytkownika("def", "sss", 45, true, "m")
+uczniowie.zapiszUzytkownika("swd", "sss", 12, false, "k")
+console.log(uczniowie.zaloguj("swd", "sss"))
+console.log(uczniowie.zaloguj("sad", "wer"))
+console.log(uczniowie.filtrujPlec("m"))
+console.log(uczniowie.filtrujPlec("k"))
+console.log(uczniowie.sortujWiekiem("asc"))
+console.log(uczniowie.sortujWiekiem("desc"))
+
+console.log(uczniowie.listujUzytkownikow())
 console.log('start servera');
 
 var express = require('express');
